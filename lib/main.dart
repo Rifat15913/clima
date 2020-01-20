1d369647840f4ae788de87dee00cc3028b5f2037
Rifat <rifat@setuptech.international>
2020-01-20 17:36:32 +0600

    1. Add location permissions, 2. Get current location for both Android and iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/loading_screen.dart';
 
 void main() => runApp(MyApp());
 
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../services/location.dart';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Location location = Location();
+    await location.getLastLocation();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
