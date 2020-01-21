eb46133be231203f2f08d617b19dc95abac2d751
Rifat <rifat@setuptech.international>
2020-01-21 11:50:02 +0600

    Get sample weather data from open weather map API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import '../services/location.dart';
 
@@ -13,6 +14,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getLastLocation();
   }
 
+  void getData() async {
+    http.Response response =
+    await http.get('https://samples.openweathermap.org/data/2.5/'
+        'weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -21,6 +34,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(
         child: RaisedButton(
